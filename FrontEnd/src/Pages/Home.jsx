e06d7c6e2ce292f6f3557d9a709afcb690e923c4
ADI-2707 <aditya21singh2707@gmail.com>
2025-12-31 13:27:44 +0530

    polished animations

--- FrontEnd/src/Pages/Home.jsx
+++ FrontEnd/src/Pages/Home.jsx
@@ -4,8 +4,8 @@ import HeroSection from "../Components/HeroSection";
 const Home = () => {
   return (
     <div>
-      <h1 className="text-center text-3xl md:text-6xl font-bold mt-6">
-        Welcome to the Home Page
+      <h1 className="text-center text-3xl md:text-6xl font-bold mt-6 hero-header">
+        Welcome to the Web App
       </h1>
       <div className="px-4 mt-20">
         <HeroSection />
